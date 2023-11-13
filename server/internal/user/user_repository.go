6f22b4add21cd8c10f6901bdedbbb394bf4072ff
ngtrgiabao <ngtrgiabao251002@gmail.com>
2023-11-13 22:05:08 +0700

    feat: add singup & login

--- server/internal/user/user_repository.go
+++ server/internal/user/user_repository.go
@@ -9,7 +9,7 @@ type DBTX interface {
 	ExecContext(ctx context.Context, query string, args ...interface{}) (sql.Result, error)
 	PrepareContext(context.Context, string) (*sql.Stmt, error)
 	QueryContext(context.Context, string, ...interface{}) (*sql.Rows, error)
-	QuerryRowContext(context.Context, string, ...interface{}) *sql.Row
+	QueryRowContext(context.Context, string, ...interface{}) *sql.Row
 }
 
 type repository struct {
@@ -23,11 +23,23 @@ func NewRepository(db DBTX) Repository {
 func (r *repository) CreateUser(ctx context.Context, user *User) (*User, error) {
 	var lastInsertId int
 	query := "INSERT INTO users(username, password, email) VALUES ($1, $2, $3) returning id"
-	err := r.db.QuerryRowContext(ctx, query, user.Username, user.Password, user.Email).Scan(&lastInsertId)
+	err := r.db.QueryRowContext(ctx, query, user.Username, user.Password, user.Email).Scan(&lastInsertId)
 	if err != nil {
 		return &User{}, err
 	}
 
 	user.ID = int64(lastInsertId)
 	return user, nil
+}
+
+func (r *repository) GetUserByEmail(ctx context.Context, email string) (*User, error) {
+	u := User{}
+	query := "SELECT id, email, username, password FROM users WHERE email = $1"
+	err := r.db.QueryRowContext(ctx, query, email).Scan(&u.ID, &u.Email, &u.Username, &u.Password)
+
+	if err != nil {
+		return &User{}, nil
+	}
+
+	return &u,nil
 }
--- server/internal/user/user_service.go
+++ server/internal/user/user_service.go
@@ -5,6 +5,12 @@ import (
 	"server/util"
 	"strconv"
 	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+const (
+	secretKey = "secretKey"
 )
 
 type service struct {
@@ -47,4 +53,43 @@ func (s *service) CreateUser(c context.Context, req *CreateUserReq) (*CreateUser
 	}
 
 	return res, nil
+}
+
+type MyJwtClaims struct {
+	ID string `json:id`
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func (s *service) Login(c context.Context, req *LoginUserReq) (*LoginUserRes, error) {
+	ctx, cancel := context.WithTimeout(c, s.timeout)
+	defer cancel()
+	u, err := s.Repository.GetUserByEmail(ctx, req.Email)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	err = util.CheckPassword(req.Password, u.Password)
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyJwtClaims{
+		ID: strconv.Itoa(int(u.ID)),
+		Username: u.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer: strconv.Itoa(int(u.ID)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(24 * time.Hour)),
+		},
+	})
+
+	ss, err := token.SignedString([]byte(secretKey))
+
+	if err != nil {
+		return &LoginUserRes{}, err
+	}
+
+	return &LoginUserRes{
+		accessToken: ss, Username: u.Username, ID: strconv.Itoa(int(u.ID)), 
+	}, nil
 }
